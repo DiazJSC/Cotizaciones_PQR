fed55244a9115f5fc044d4e90d3be3de9d6757f7
DiazJSC <diazjuanse22@gmail.com>
2024-10-16 22:28:10 -0500

    Se arregla bug en el test de cotizacion

--- tests/test_cotizacion.py
+++ tests/test_cotizacion.py
@@ -6,16 +6,11 @@ from app.ventana import Ventana
 
 def test_descuento_empresa():
     cliente = "Juan Diaz"
-    ventana = Ventana("O", 12.0, 15.0, "Pulido", "Transparente", "N")
+    ventana = Ventana("O", 12.0, 15.0, "Pulido", "Transparente", False)
     cant_ventanas = 102
 
     cotizacion = Cotizacion(cliente, ventana, cant_ventanas)
 
     descuento_esperado = 3060703.8
 
-    #assert cotizacion.calcular_total() == descuento_esperado
-    print(f"Costo aluminio: {ventana.calcular_valor_aluminio()}")
-    print(f"Costo vidrio: {ventana.calcular_valor_vidrio()}")
-    print(f"Costo esquinas: {ventana.calcular_valor_esquinas()}")
-    print(f"Costo accesorios: {ventana.calcular_valor_accesorios()}")
-    assert abs(cotizacion.calcular_total() - descuento_esperado) < 0.01
+    assert cotizacion.calcular_total() == descuento_esperado
